3e5fc7530b2099cf1a15e644de3104f2bf7b9baf
michv <michv@LAPTOP-032F5TDQ>
2024-03-07 09:35:22 +0300

    Сделал класс group

--- ClassLibrary/Group.cs
+++ ClassLibrary/Group.cs
@@ -8,5 +8,27 @@ namespace ClassLibrary
 {
     internal class Group
     {
+        private string name;
+        private string shortname;
+        private int quantity;
+        private int year;
+        private Speciality speciality;
+        private Employee employee;
+        public Group(string name, string shortname, int quantity, Speciality speciality, Employee employee, int year = -1)
+        {
+            this.name = name;
+            this.shortname = shortname;
+            this.quantity = quantity;
+            this.speciality = speciality;
+            this.employee = employee;
+            if (year == -1)
+            {
+                this.year = DateTime.Now.Year;
+            }
+            else
+            {
+                this.year = year;
+            }
+        }
     }
 }
