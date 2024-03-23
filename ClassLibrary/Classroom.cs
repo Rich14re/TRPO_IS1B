354b7f38a70faa526d920e2bcf086ffde2bdbd91
ksuha <ksuha11kiseleva@gmail.com>
2024-03-23 15:12:28 +0300

    добавлена ссылка(на оборудование)

--- ClassLibrary/Classroom.cs
+++ ClassLibrary/Classroom.cs
@@ -7,24 +7,41 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
+    /// <summary>
+    /// https://docs.google.com/document/d/10XwtkjNB6-lRehWP_7qbgWC1nn_oIpa28ezyPXX3B28/edit 2 вариант
+    /// </summary>
     internal class Classroom
     {
-        private string date;
         private string name;
         private Employee employee;
-        private string window;
+        private int places;
+        private int windows;
         private Equipment equipment;
-        private string seat;
-        public Classroom(string date, string name, Employee employee, string window, Equipment equipment, string seat)
+        public Classroom(string name, Employee employee, int count_places, int count_windows, Equipment equipment)
         {
-            this.date = DateTime.Today.ToString();
             this.name = name;
             this.employee = employee;
-            this.window = window;
+            this.places = count_places;
+            this.windows = count_windows;
             this.equipment = equipment;
-            this.seat = seat;
         }
 
+        public string Number
+        {
+            get { return name; }
+        }
+
+
+        public int Places
+        {
+            get { return places; }
+        }
+
+        public int Windows
+        {
+            get { return windows; }
+        }
     }
-    
+
 }
+
