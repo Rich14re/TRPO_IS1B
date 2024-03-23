36eb96eb6662942248cb4a2a21aac66ee72c8431
vasilisa30 <nikitinavasilisa30@gmail.com>
2024-03-23 13:51:25 +0300

    сделано

--- ClassLibrary/Employee.cs
+++ ClassLibrary/Employee.cs
@@ -8,5 +8,17 @@ namespace ClassLibrary
 {
     internal class Employee
     {
+        private string name;
+        private string surname;
+        private string patronymic;
+        private Position position;
+
+        public Employee(string name, string surname, string patronymic, Position position)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic; 
+            this.position = position;
+        }
     }
 }
