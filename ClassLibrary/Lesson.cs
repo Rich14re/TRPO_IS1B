e5ed4bd172b3e13977453f40e8a32a97453477a8
Kover Porshe <razvalov2016@yandex.ru>
2024-03-23 15:55:52 +0300

    Добавил конструкторы

--- ClassLibrary/Lesson.cs
+++ ClassLibrary/Lesson.cs
@@ -6,26 +6,37 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class Lesson
+    /// <summary>
+    /// https://docs.google.com/document/d/1KZXfKJussGflgWnWylGbFrwmdcn3liYKIsgmtxB1mN0/edit
+    /// </summary>
+    public class Lesson
     {
-        private DateTime dateHeld;
-        private Discipline disciplineLink;
-        private Employee employeeLink;
-        private Classroom classroomLink;
-        private Group groupLink;
-        private Pair pairLink;
-        private Lesson lessonTypeLink;
+        private readonly DateTime dateHeld;
+        private readonly Discipline discipline;
+        private readonly Employee employee;
+        private readonly Classroom classroom;
+        private readonly Group group;
+        private readonly Pair pair;
+        private readonly TypeOfActivity typeOfActivity;
 
-        public Lesson(DateTime dateHeld, Discipline disciplineLink, Employee employeeLink, Classroom classroomLink, Group groupLink, Pair pairLink, Lesson lessonTypeLink)
+        public Lesson(DateTime dateHeld, Discipline discipline, Employee employee, Classroom classroom, Group group, Pair pair, TypeOfActivity lessonType)
         {
             this.dateHeld = dateHeld != default ? dateHeld : DateTime.Now;
-            this.disciplineLink = disciplineLink;
-            this.employeeLink = employeeLink;
-            this.classroomLink = classroomLink;
-            this.groupLink = groupLink;
-            this.pairLink = pairLink;
-            this.lessonTypeLink = lessonTypeLink;
+            this.discipline = discipline;
+            this.employee = employee;
+            this.classroom = classroom;
+            this.group = group;
+            this.pair = pair;
+            this.typeOfActivity = lessonType;
         }
+
+        public DateTime DateHeld => dateHeld;
+        public Discipline Discipline => discipline;
+        public Employee Employe => employee;
+        public Classroom Classroom => classroom;
+        public Group Group => group;
+        public Pair Pair => pair;
+        public TypeOfActivity TypeOfActivity => typeOfActivity;
     }
 }
 
