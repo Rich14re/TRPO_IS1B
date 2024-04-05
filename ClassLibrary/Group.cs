9a5d043c635ad42405533e585616ea6f638f07b5
michv <michv@LAPTOP-032F5TDQ>
2024-04-05 18:38:21 +0300

    Изменение 2

--- ClassLibrary/Group.cs
+++ ClassLibrary/Group.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ClassLibrary
 {
     internal class Group
     {
+        private readonly string name;
+        private readonly string shortname;
+        private readonly int quantity;
+        private readonly int year;
+        private readonly Speciality speciality;
+        private readonly Employee employee;
         //Изменения 1
-        private string name;
-        private string shortname;
-        private int quantity;
-        private int year;
-        private Speciality speciality;
-        private Employee employee;
-        public Group(string name, string shortname, int quantity, Speciality speciality, Employee employee, int year = -1)
+        public Group(string name, string shortname, int quantity, Speciality speciality, Employee classroomteatcher, int year = -1)
         {
             this.name = name;
             this.shortname = shortname;
@@ -30,6 +32,35 @@ namespace ClassLibrary
             {
                 this.year = year;
             }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Shortname
+        {
+            get { return shortname; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+        }
+
+        public Speciality Speciality
+        {
+            get { return speciality; }
+        }
+
+        public Employee Employee
+        {
+            get { return employee; }
         }
     }
 }
