16325b9dbfc99e71ca9503c65c454038b9d187eb
Kover Porshe <razvalov2016@yandex.ru>
2024-04-05 23:00:04 +0300

    B6Speci

--- ClassLibrary/Speciality.cs
+++ ClassLibrary/Speciality.cs
@@ -6,19 +6,28 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class Speciality
+    public class Speciality
     {
+        /// <summary>
+        /// https://docs.google.com/document/d/1pDw7bbBJDmeqT8VVwp68BSOF_4yd9cNb9xpH_q2KW0E/edit
+        /// </summary>
+        private readonly string name;
+        private readonly string abbreviation;
 
-        private string name;
-        private string abbreviation;
-        private string date;
-
-        public Speciality(string name, string abbreviation, string date)
+        public Speciality(string name, string abbreviation)
         {
             this.name = name;
             this.abbreviation = abbreviation;
-            this.date = DateTime.Today.ToString();
+        }
 
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Abbreviation
+        {
+            get { return abbreviation; }
         }
     }
 }
