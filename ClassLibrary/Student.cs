92f8cf130465cf5239950410e9f6ef8ae829aad3
julyll <murjulianikova@mail.ru>
2024-04-05 18:03:58 +0300

    сделали класс

--- ClassLibrary/Student.cs
+++ ClassLibrary/Student.cs
@@ -8,5 +8,26 @@ namespace ClassLibrary
 {
     internal class Student
     {
+        private string surname; 
+        private string name;
+        private string patronymic; 
+        private Group group;
+        private string birth; 
+        public Student(string surname, string name, string patronymic, Group group, string birth)
+        {
+            this.surname = surname;
+            this.name = name; 
+            this.patronymic = patronymic;
+            this.group = group; 
+            if (birth != null)
+            {
+                this.birth = birth;
+            }
+            else
+            {
+                this.birth = DateTime.Today.ToString();
+            }
+        }
     }
 }
+
