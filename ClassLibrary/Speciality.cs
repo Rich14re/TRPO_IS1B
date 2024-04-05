774361fcbcc0c49c927b62ea107fda60de75ebd0
nastyya <nastiaponomarenko.anastasiya@yandex.ru>
2024-04-05 21:04:19 +0300

    всё супер

--- ClassLibrary/Speciality.cs
+++ ClassLibrary/Speciality.cs
@@ -8,5 +8,17 @@ namespace ClassLibrary
 {
     internal class Speciality
     {
+
+        private string name;
+        private string abbreviation;
+        private string date;
+
+        public Speciality(string name, string abbreviation, string date)
+        {
+            this.name = name;
+            this.abbreviation = abbreviation;
+            this.date = DateTime.Today.ToString();
+
+        }
     }
 }
